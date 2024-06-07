15a6d95dd4cf547693aea918f1f99e7491b9b09e
binyoma <innocentbinyoma@pm.me>
2024-06-07 09:12:28 +0200

    before going back to templating

--- web/front.php
+++ web/front.php
@@ -7,29 +7,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
-function render_template(Request $request): Response
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+// example.com/web/front.php
 
-    return new Response(ob_get_clean());
-}
-
+// ...
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func($request->attributes->get('_controller'), $request);
-} catch (Routing\Exception\ResourceNotFoundException $exception) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 
 $response->send();
